d7506393ea317392096531afec7d6ca001ee1ec2
James McCormack <thejamesmccormack@gmail.com>
2018-04-27 12:41:48 -0700

    change back url

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: 'https://dragon-picnic.herokuapp.com/',
+  origin: 'https://dragon-picnic.herokuapp.com',
   originSuccessStatus: 200,
 };
 
